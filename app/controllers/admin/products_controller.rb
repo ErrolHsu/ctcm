7545175d8ea63ec2bc2b1369e15ce0114f4db7a6
zjhuang <zjhuang@ZJteki-MacBook-Pro.local>
2018-05-09 02:21:20 +0800

    Product crud done

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,48 @@
 class Admin::ProductsController < AdminController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      # 成功
+      redirect_to admin_products_path
+    else
+      # 失敗
+      render :new
+    end
+  end 
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end  
+
+  def update
+    @product = Product.find_by(id: params[:id])
+
+    if @product.update(product_params)
+      # 成功
+      redirect_to admin_products_path
+    else
+      # 失敗
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy if @product
+    redirect_to admin_products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :quantity)
+  end  
 end
