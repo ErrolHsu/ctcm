2093036fc1f8dfbaac03e7c8255667352aefd70d
VinHuang <shadowmirror@gmail.com>
2018-05-13 02:43:59 +0800

    購物車 part 1

---
+++ app/models/cart.rb
@@ -0,0 +1,22 @@
+class Cart
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  def add_item(product_id)
+    found_item = items.find { |item| item.product_id == product_id }
+
+    if found_item
+      found_item.increment
+    else
+      @items << CartItem.new(product_id)
+    end
+  end
+
+  def empty?
+    @items.empty?
+  end
+
+end
