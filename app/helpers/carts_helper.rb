8891be768beeb3085fcc777eb915a2164676044f
s20a3264 <s20a3264@hotmail.com>
2018-05-18 00:16:46 +0800

    購物車合併

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -15,11 +15,19 @@ module CartsHelper
     cart
   end
 
+  # 有登入時的購物車
   def find_user_cart
-    cart = Cart.find_by(user_id: current_user.id)
+    cart = current_user.cart
     if cart.blank?
       cart = Cart.find_by(id: session[:cart_id])
       cart.update_attributes(user_id: current_user.id)
+    # 如果舊購物車存在，合併新舊購物車
+    elsif (cart.id != session[:cart_id])
+      new_cart = Cart.find_by(id: session[:cart_id])
+      new_cart_products = new_cart.products.map(&:id)
+      new_cart_products.each {|product| cart.add_item(product)}
+      new_cart.destroy
+      cart
     end
     cart
   end
