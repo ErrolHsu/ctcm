aa2887f129e96bef7b54f483ef647aee17c2f113
s20a3264 <s20a3264@hotmail.com>
2018-06-02 15:04:07 +0800

    move products list

--- config/routes.rb
+++ config/routes.rb
@@ -2,26 +2,19 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
 
-  # # admin 登入後 root
-  # authenticated :admin do
-  #   root 'admin/core#index', as: :authenticated_root
-  # end
-
   resource :cart, only:[:show, :destroy] do
     collection do
       post :add, path:'add/:id'
     end
   end
 
+  resources :products
+
   namespace :admin do
     root 'core#index'
     get 'index' => 'core#index'
     resources :products
   end
-  # 顧客登入後 root
-  # authenticated :user do
-  #   root ''
-  # end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
