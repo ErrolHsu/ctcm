66e4f0135e2ceb0c4c8b343f26fe179932a8342d
s20a3264 <s20a3264@hotmail.com>
2018-06-03 16:41:41 +0800

    add article controller & model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
 
+  root 'home#index'
+  get 'home/ajax' => 'home#ajax'
+
   resource :cart, only:[:show, :destroy] do
     collection do
       post :add, path:'add/:id'
@@ -14,10 +17,7 @@ Rails.application.routes.draw do
     root 'core#index'
     get 'index' => 'core#index'
     resources :products
+    resources :articles
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  get 'home/ajax' => 'home#ajax'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180602163307) do
+ActiveRecord::Schema.define(version: 20180603083155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20180602163307) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "cart_items", force: :cascade do |t|
