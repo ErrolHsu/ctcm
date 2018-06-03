6ddee4d762d1ffca8969fd8b136d25e19fdb60f9
s20a3264 <s20a3264@hotmail.com>
2018-06-03 17:36:53 +0800

    article crud

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,2 +1,43 @@
 class Admin::ArticlesController < AdminController
+  expose :articles, -> { Article.all }
+  expose :article
+
+  def index
+  end
+
+  def new
+
+  end
+
+  def create
+    if article.save
+      redirect_to admin_articles_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if article.update(article_params)
+      redirect_to admin_articles_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    article.destroy
+    redirect_to admin_articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
 end
