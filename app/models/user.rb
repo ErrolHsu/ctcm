f77da8f37d62ce874da4f7f370de2f174bc0c484
ian-yei <iansrc0811@gmail.com>
2018-06-13 23:57:18 +0800

    fix user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,15 +12,12 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      # byebug
       user.provider = auth.provider
       user.uid      = auth.uid
       user.email    = auth.info.email
       user.name     = auth.info.name
-      # user.facebook = auth.info.urls.Facebook
       user.password = Devise.friendly_token[0,20]
       user.remote_avatar_url   = auth.info.image
-      # user.skip_confirmation!  # 如果 devise 有使用 confirmable，記得 skip！
     end
   end
 
