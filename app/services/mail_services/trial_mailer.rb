b8a30d68765345cd7914129c55907ed0cc17e1ba
s20a3264 <s20a3264@hotmail.com>
2018-08-19 03:16:38 +0800

    add reject mail content

--- app/services/mail_services/trial_mailer.rb
+++ app/services/mail_services/trial_mailer.rb
@@ -3,7 +3,25 @@ module MailServices
 
     def self.reject(trial)
       subject = 'SORRY'
-      text = "......."
+      text = <<-heredoc
+        您好：
+
+        感謝您參與Altitude Tosteria的開幕系列活動，
+        我們的試喝包已經索取完畢。
+
+        請持續關注我們，
+        並推薦您的朋友認識Altitude Tosteria，
+        我們不定期會有優惠活動，
+        也即將在九月份隆重開幕，
+        敬請期待！
+
+        http://www.instagram.com/altitudetosteriacafe/
+
+        All the best,
+        Afdel Bernal
+        CEO / Co-Founder, Altitude Tosteria
+      heredoc
+
       send(to: trial.email, subject: subject, text: text)
     end
 
