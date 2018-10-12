855eee28a2cf6a812d62dc38d4b6b3b7e3581bb2
s20a3264 <s20a3264@hotmail.com>
2018-10-13 00:16:27 +0800

    fix bug

--- app/jobs/mail_worker/admin/period_order_cancel_job.rb
+++ app/jobs/mail_worker/admin/period_order_cancel_job.rb
@@ -1,10 +1,12 @@
 module MailWorker
-  class Admin::PeriodOrderCancelJob < ApplicationJob
-    queue_as :mailers
+  module Admin
+    class PeriodOrderCancelJob < ApplicationJob
+      queue_as :mailers
 
-    def perform(order_id)
-      order = Order.find_by(id: order_id)
-      MailServices::Admin::OrderMailer.period_order_cancel(order)
+      def perform(order_id)
+        order = Order.find_by(id: order_id)
+        MailServices::Admin::OrderMailer.period_order_cancel(order)
+      end
     end
   end
 end
