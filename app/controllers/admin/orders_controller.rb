44731deebbf7f4d2c169eeb6a8be669f4f1c8dbb
s20a3264 <s20a3264@hotmail.com>
2018-10-22 20:52:20 +0800

    前端調整

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,7 @@ class Admin::OrdersController < AdminController
 
   expose(:orders) { Order.all }
   expose(:order)
+  expose(:order_json) { order.to_json(except: [:id, :created_at, :updated_at], methods: [:status_name, :payment_name, :type]) }
   expose(:current_period_order) { order.period_orders.where(status: [:open, :pending]).last }
   expose(:next_period_order) { order.period_orders.where(status: [:future]).last }
 
