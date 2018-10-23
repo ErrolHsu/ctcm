1fc6038777504ff56cc423431173673aaed99b9a
s20a3264 <s20a3264@hotmail.com>
2018-10-23 22:12:32 +0800

    refine code

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,7 +3,7 @@ class Admin::OrdersController < AdminController
   expose(:orders) { Order.all }
   expose(:order)
   expose(:order_json) { order.to_json(except: [:id, :created_at, :updated_at], methods: [:status_name, :payment_name, :type]) }
-  expose(:current_period_order) { order.period_orders.where(status: [:open, :pending]).last }
+  expose(:current_period_order) { order.current_period_order }
   expose(:current_period_order_json) { order.current_period_order_json }
   expose(:next_period_order) { order.period_orders.where(status: [:future]).last }
 
@@ -17,8 +17,6 @@ class Admin::OrdersController < AdminController
   # 當期定期訂單改為烘培中
   def period_order_preparing
     order = Order.find_by(order_no: params['id'])
-
-    current_period_order = order.current_period_order
 
     unless current_period_order.try(:paid)
       render json: { message: '本期訂單尚未付款' }, status: 400
