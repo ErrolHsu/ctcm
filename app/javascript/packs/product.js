390568626e7d16af5dfbdb7c12b7dc1666fe8e8b
Errol <s20a3264@hotmail.com>
2018-12-06 21:02:39 +0800

    商城頁

---
+++ app/javascript/packs/product.js
@@ -0,0 +1,33 @@
+// 商城頁跟商品頁都用這支
+import Vue from 'vue/dist/vue.esm';
+import axios from 'axios'
+import { vue_init } from '../mixins/vue_init.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const productPageApp = new Vue({
+    el: '#product-page-app',
+    mixins: [vue_init],
+    data: {
+
+    },
+
+    components: {
+
+    },
+
+    mounted() {
+
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+
+
+    },
+
+
+  });
+});
